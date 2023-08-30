32775104e6972f89ec8f510ece162b5c08112903
Daniela Füchtenbusch <119465040+fuctenbusch@users.noreply.github.com>
2023-08-30 11:27:20 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,223 +2,137 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
 void main() {
-  runApp(const MyApp());
+  runApp(CronometroApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class CronometroApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeApp(),
+      title: 'Cronômetro App',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      home: CronometroScreen(),
     );
   }
 }
 
-class HomeApp extends StatefulWidget {
-  const HomeApp({Key? key}) : super(key: key);
-
+class CronometroScreen extends StatefulWidget {
   @override
-  _HomeAppState createState() => _HomeAppState();
+  _CronometroScreenState createState() => _CronometroScreenState();
 }
 
-class _HomeAppState extends State<HomeApp{
+class _CronometroScreenState extends State<CronometroScreen> {
+  bool isRunning = false;
+  int seconds = 0;
+  List<String> marks = [];
 
-  int seconds = 0, minutes = 0, hours = 0;
-  String digitSeconds = "00", digitMinutes = "00", digitHours = "00";
-  Timer? timer;
-  bool started = false;
-  List laps = [];
-
-  void stop(){
-    timer!.cancel();
+  void startTimer() {
     setState(() {
-      started = false;
+      isRunning = true;
     });
-  }
 
-
-  void reset(){
-    timer!.cancel();
-    setState(() {
-      seconds = 0;
-      minutes = 0;
-      hours = 0;
-
-      digitSeconds = "00";
-      digitMinutes = "00";
-      digitHours = "00";
-
-      started = false;
-    });
-  }
-
-  void addLaps() {
-    String lap = "$digitHours:$digitMinutes:$digitSeconds";
-    setState(() {
-      laps.add(lap);
-    });
-  }
-
-  void start() {
-    started = true;
-    timer = Timer.periodic(Duration(seconds: 1), (timer){ 
-      int localSeconds = seconds +1;
-      int localMinutes = minutes;
-      int localHours = hours;
-
-      if (localSeconds > 59){
-        if (localMinutes > 59){
-          localHours++;
-          localMinutes = 0;
-        }else {
-          localMinutes++;
-          localSeconds = 0;
-        }
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      if (!isRunning) {
+        timer.cancel();
+      } else {
+        setState(() {
+          seconds++;
+        });
       }
-      setState((){
-        seconds = localSeconds;
-        minutes = localMinutes;
-        hours = localHours;
-        digitSeconds = (seconds >= 10) ? "$seconds" : "$seconds";
-        digitHours = (hours >= 10) ? "$hours" : "$hours";
-        digitMinutes = (minutes >= 10) ? "$minutes" : "$minutes";
-      });
     });
+  }
+
+  void stopTimer() {
+    setState(() {
+      isRunning = false;
+    });
+  }
+
+  void markTime() {
+    setState(() {
+      marks.add(_formatTime(seconds));
+    });
+  }
+
+  void resetTimer() {
+    setState(() {
+      isRunning = false;
+      seconds = 0;
+      marks.clear();
+    });
+  }
+
+  String _formatTime(int timeInSeconds) {
+    int minutes = timeInSeconds ~/ 60;
+    int seconds = timeInSeconds % 60;
+    return '$minutes:${seconds.toString().padLeft(2, '0')}';
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF1C2757),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(
-                child: Text(
-                  "StopWatch App",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold,
-                    ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 400.0,
-                decoration: BoxDecoration(
-                  color: Color(0xFF323F68),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Center(
-                child: 
-                Text("$digitHours:$digitMinutes:$digitSeconds",
-                style: TextStyle(
-                  color: Colors.white,
-                    fontSize: 82.0,
-                    fontWeight: FontWeight.w600,
-                ),
-              ),
-              ),
-              Container(
-                height: 400.0,
-                decoration: BoxDecoration(
-                  color: Color(0xFF32F68),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: ListView.builder(
-                  itemCount: laps.length,
-                  itemBuilder: (context, index){
-                    return Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Lap n${index+1}",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16.0, 
-                            ),
-                          ),
-                          Text(
-                            "${laps[index]}",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                      );
-                  },
-                  ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: RawMaterialButton(
-                    onPressed: () {
-                      (started) ?start():stop();
-                    },
-                    shape: const StadiumBorder(
-                     side: BorderSide(color: Colors.blue),
-                    ),
-                    child: Text(
-                      (started) ? "Start" : "Pause",
-                      style: TextStyle(color: Colors.white),
-                    ), 
-                    ),
-                  ),
-                  SizedBox(
-                    width: 8.0,
-                  ),
-                  IconButton(
-                    color: Colors.white,
-                    onPressed: () {
-                      addLaps();
-                    }, 
-                    icon: Icon(Icons.flag),
-                    ),
-                    SizedBox(
-                      width: 8.0,
-                    ),
-                  Expanded(
-                    child: RawMaterialButton(
-                      onPressed: () {
-                        reset();
-                      },
-                      fillColor: Colors.blue,
-                      shape: const StadiumBorder(),
-                      child: Text(
-                        "Reset",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      ),
-                  ),
-                ],
-              ),
-            ],
+      appBar: AppBar(
+        title: Text('Cronômetro'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _formatTime(seconds),
+              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
             ),
-          ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                if (!isRunning)
+                  ElevatedButton(
+                    onPressed: startTimer,
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.black,
+                    ),
+                    child:
+                        Text('INICIAR', style: TextStyle(color: Colors.white)),
+                  ),
+                if (isRunning)
+                  ElevatedButton(
+                    onPressed: stopTimer,
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.grey,
+                    ),
+                    child: Text('PARAR', style: TextStyle(color: Colors.white)),
+                  ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: markTime,
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.brown,
+                  ),
+                  child: Text('MARCAR', style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: resetTimer,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.brown,
+              ),
+              child: Text('LIMPAR', style: TextStyle(color: Colors.white)),
+            ),
+            SizedBox(height: 20),
+            Column(
+              children: marks
+                  .map((mark) => Text(mark, style: TextStyle(fontSize: 20)))
+                  .toList(),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
